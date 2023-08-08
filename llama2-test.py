4a73aa1aa1944db674ce6b2193e3ae3ef1392e1a
kawachou <kawachou@gmail.com>
2023-08-08 08:02:44 +0000

    微修正

--- llama2-test.py
+++ llama2-test.py
@@ -3,16 +3,11 @@ import os
 
 # localで実行中のllama2
 openai.api_base = "http://host.docker.internal:8000/v1"
-# openai.api_version = "2023-06-01-preview"
 
 def generate_text(message):
     completion = openai.ChatCompletion.create(
         # Llma2の場合　（文字列は適当でも動くっぽい）
         model="/models/llama-2-7b-chat.ggmlv3.q5_K_M.bin",
-        # Azure OpenAI gpt-4の場合
-        # deployment_id = "gpt-4",
-        # Azure OpenAI gpt-3.5-turboの場合
-        # deployment_id = "gpt-3.5-turbo",
         messages = [
             {
                 "role": "user",
